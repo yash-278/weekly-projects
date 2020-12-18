3c6e90d955bb6c1575afd264b590096245912a99
Yash Kadam <yashkadam278@gmail.com>
2020-12-18 11:03:21 +0530

    QrCode Generator

--- QR-Generator/app.js
+++ QR-Generator/app.js
@@ -1,8 +1,8 @@
-var typeNumber = 0;
-var errorCorrectionLevel = "L";
-var qr = qrcode(typeNumber, errorCorrectionLevel);
-
-function myFunction() {
+// var typeNumber = 0;
+// var errorCorrectionLevel = "L";
+// var qr = qrcode(typeNumber, errorCorrectionLevel);
+var qrCode = new QRCode(document.getElementById("qrcode"));
+function Qrcode() {
   var x = document.getElementById("qrForm");
   var text = "";
   var i;
@@ -10,10 +10,20 @@ function myFunction() {
   for (i = 0; i < x.length; i++) {
     text += `${x.elements[i].name} : ${x.elements[i].value}, `;
   }
-
+  return text;
   // console.log(text);
-  qr.addData(text);
-  qr.make();
-  document.getElementById("placeHolder").innerHTML = qr.createImgTag(4);
+
   // document.getElementById("demo").innerHTML = text;
+}
+
+function generateCode() {
+  //   let QrText = Qrcode();
+
+  //   qr.addData(QrText);
+  //   qr.make();
+  //   document.getElementById("placeHolder").innerHTML = qr.createImgTag(4);
+
+  var qrData = Qrcode();
+
+  qrCode.makeCode(qrData);
 }
