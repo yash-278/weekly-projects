840256cc43f57c2fe50ac9ada24b9611c20a5580
Yash Kadam <yashkadam278@gmail.com>
2020-12-18 11:04:58 +0530

    QrCode Generator

--- QR-Generator/app.js
+++ QR-Generator/app.js
@@ -1,7 +1,5 @@
-// var typeNumber = 0;
-// var errorCorrectionLevel = "L";
-// var qr = qrcode(typeNumber, errorCorrectionLevel);
 var qrCode = new QRCode(document.getElementById("qrcode"));
+
 function Qrcode() {
   var x = document.getElementById("qrForm");
   var text = "";
@@ -11,19 +9,9 @@ function Qrcode() {
     text += `${x.elements[i].name} : ${x.elements[i].value}, `;
   }
   return text;
-  // console.log(text);
-
-  // document.getElementById("demo").innerHTML = text;
 }
 
 function generateCode() {
-  //   let QrText = Qrcode();
-
-  //   qr.addData(QrText);
-  //   qr.make();
-  //   document.getElementById("placeHolder").innerHTML = qr.createImgTag(4);
-
   var qrData = Qrcode();
-
   qrCode.makeCode(qrData);
 }
